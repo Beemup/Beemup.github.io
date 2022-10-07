e1be905b3bfb341791d9e36098737f5451130715
KungFuDonkey <sietze.riemersma@gmail.com>
2022-10-07 08:08:24 +0000

    update of documentation

--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['about_20yomplugins_0',['About YomPlugins',['../index.html',1,'']]],
+  ['about_0',['About',['../index.html',1,'']]],
   ['advanced_20inventory_20setup_1',['Advanced Inventory Setup',['../_inventory_setup.html',1,'md_pages_Guides']]],
   ['advanced_20player_20setup_2',['Advanced Player Setup',['../_player_setup.html',1,'md_pages_Guides']]]
 ];

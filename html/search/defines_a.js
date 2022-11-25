38484458578108bea1dab06d7a2d77267b1ccd30
KungFuDonkey <sietze.riemersma@gmail.com>
2022-11-25 13:28:53 +0000

    update of documentation

--- html/search/defines_a.js
+++ html/search/defines_a.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['signalling_5fsample_5frate_0',['SIGNALLING_SAMPLE_RATE',['../_yom_audio_recorder_component_8cpp.html#a58089ae56fa99093c060e17bf13c77f3',1,'YomAudioRecorderComponent.cpp']]],
-  ['stop_5ftalking_5fthreshold_1',['STOP_TALKING_THRESHOLD',['../_yom_audio_recorder_component_8cpp.html#a88303c0124beb0af64a551a35128721a',1,'YomAudioRecorderComponent.cpp']]],
-  ['string_5fsetting_2',['STRING_SETTING',['../_yom_settings_window_8cpp.html#a6a4c06c7618a86c78a91c5da20ff3ca0',1,'YomSettingsWindow.cpp']]]
+  ['stop_5ftalking_5fthreshold_0',['STOP_TALKING_THRESHOLD',['../_yom_audio_recorder_component_8cpp.html#a88303c0124beb0af64a551a35128721a',1,'YomAudioRecorderComponent.cpp']]],
+  ['string_5fsetting_1',['STRING_SETTING',['../_yom_settings_window_8cpp.html#a6a4c06c7618a86c78a91c5da20ff3ca0',1,'YomSettingsWindow.cpp']]]
 ];

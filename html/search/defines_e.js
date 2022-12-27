c2ed03e7f046b9ff629fafc48ee89f8bc74e2db3
KungFuDonkey <sietze.riemersma@gmail.com>
2022-12-27 12:15:55 +0000

    update of documentation

--- html/search/defines_e.js
+++ html/search/defines_e.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['with_5fyom_5fvoice_5fsettings_0',['WITH_YOM_VOICE_SETTINGS',['../_yom_settings_window_macros_8h.html#a5bd149b56f482a42c0feabedbaa5a556',1,'YomSettingsWindowMacros.h']]]
+  ['with_5fyom_5fvoice_5fsettings_0',['WITH_YOM_VOICE_SETTINGS',['../_yom_package_command_macros_8h.html#a5bd149b56f482a42c0feabedbaa5a556',1,'WITH_YOM_VOICE_SETTINGS():&#160;YomPackageCommandMacros.h'],['../_yom_settings_window_macros_8h.html#a5bd149b56f482a42c0feabedbaa5a556',1,'WITH_YOM_VOICE_SETTINGS():&#160;YomSettingsWindowMacros.h']]]
 ];

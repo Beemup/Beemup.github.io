3d0951b88ec1695b335dd9a744026df8ed74fb5d
KungFuDonkey <sietze.riemersma@gmail.com>
2023-01-03 15:45:50 +0000

    update of documentation

--- html/md_pages__guides.js
+++ html/md_pages__guides.js
@@ -1,95 +1,100 @@
 var md_pages__guides =
 [
     [ "Guides", "md_pages__guides.html#autotoc_md0", null ],
+    [ "Performance Recommendations", "_performance_recommendations.html", [
+      [ "Performance Recommendations", "_performance_recommendations.html#autotoc_md1", [
+        [ "General guidelines", "_performance_recommendations.html#autotoc_md2", null ]
+      ] ]
+    ] ],
     [ "Metaspace Setup", "_metaspace_setup.html", [
-      [ "Setting up your Metaspace", "_metaspace_setup.html#autotoc_md1", [
-        [ "Prerequisites", "_metaspace_setup.html#autotoc_md2", null ],
-        [ "Step 1: Enabling plugins", "_metaspace_setup.html#autotoc_md3", null ],
-        [ "Step 2: Setting up", "_metaspace_setup.html#autotoc_md4", null ],
-        [ "Step 3: Setting your space up for debugging", "_metaspace_setup.html#autotoc_md5", null ],
-        [ "Result", "_metaspace_setup.html#autotoc_md6", null ]
+      [ "Setting up your Metaspace", "_metaspace_setup.html#autotoc_md3", [
+        [ "Prerequisites", "_metaspace_setup.html#autotoc_md4", null ],
+        [ "Step 1: Enabling plugins", "_metaspace_setup.html#autotoc_md5", null ],
+        [ "Step 2: Setting up", "_metaspace_setup.html#autotoc_md6", null ],
+        [ "Step 3: Setting your space up for debugging", "_metaspace_setup.html#autotoc_md7", null ],
+        [ "Result", "_metaspace_setup.html#autotoc_md8", null ]
       ] ]
     ] ],
     [ "Debugging your metaspace", "_debugging_metaspace.html", [
-      [ "Debugging your metaspace", "_debugging_metaspace.html#autotoc_md7", [
-        [ "Settings", "_debugging_metaspace.html#autotoc_md8", null ],
-        [ "Check the Log", "_debugging_metaspace.html#autotoc_md9", null ]
+      [ "Debugging your metaspace", "_debugging_metaspace.html#autotoc_md9", [
+        [ "Settings", "_debugging_metaspace.html#autotoc_md10", null ],
+        [ "Check the Log", "_debugging_metaspace.html#autotoc_md11", null ]
       ] ]
     ] ],
     [ "Advanced Player Setup", "_player_setup.html", [
-      [ "Additional settings for a Player", "_player_setup.html#autotoc_md10", [
-        [ "Player Asset", "_player_setup.html#autotoc_md11", null ],
-        [ "Manual Spawn Location", "_player_setup.html#autotoc_md12", null ],
-        [ "Level Sequence (virtual camera)", "_player_setup.html#autotoc_md13", null ]
+      [ "Additional settings for a Player", "_player_setup.html#autotoc_md12", [
+        [ "Player Asset", "_player_setup.html#autotoc_md13", null ],
+        [ "Manual Spawn Location", "_player_setup.html#autotoc_md14", null ],
+        [ "Level Sequence (virtual camera)", "_player_setup.html#autotoc_md15", null ]
       ] ]
     ] ],
     [ "Advanced Inventory Setup", "_inventory_setup.html", [
-      [ "Setting up an inventory", "_inventory_setup.html#autotoc_md14", [
-        [ "Step 1: Creating your own buttons", "_inventory_setup.html#autotoc_md15", [
-          [ "Tips", "_inventory_setup.html#autotoc_md16", null ]
-        ] ],
-        [ "Step 2: Creating your own Inventory Widget", "_inventory_setup.html#autotoc_md17", [
+      [ "Setting up an inventory", "_inventory_setup.html#autotoc_md16", [
+        [ "Step 1: Creating your own buttons", "_inventory_setup.html#autotoc_md17", [
           [ "Tips", "_inventory_setup.html#autotoc_md18", null ]
         ] ],
-        [ "Step 3: Creating a component", "_inventory_setup.html#autotoc_md19", null ],
-        [ "Step 4: Setting your inventory in your player", "_inventory_setup.html#autotoc_md20", null ]
+        [ "Step 2: Creating your own Inventory Widget", "_inventory_setup.html#autotoc_md19", [
+          [ "Tips", "_inventory_setup.html#autotoc_md20", null ]
+        ] ],
+        [ "Step 3: Creating a component", "_inventory_setup.html#autotoc_md21", null ],
+        [ "Step 4: Setting your inventory in your player", "_inventory_setup.html#autotoc_md22", null ]
       ] ]
     ] ],
     [ "Nameplate Setup", "_nameplate_setup.html", [
-      [ "Adding a nameplate to your character", "_nameplate_setup.html#autotoc_md21", [
-        [ "Step 1: Creating a custom nameplate", "_nameplate_setup.html#autotoc_md22", null ],
-        [ "Step 2: Adding the nameplate to your character", "_nameplate_setup.html#autotoc_md23", null ],
-        [ "Step 3: Add the YomNameplateManager to your MetaspacePlayer", "_nameplate_setup.html#autotoc_md24", null ]
+      [ "Adding a nameplate to your character", "_nameplate_setup.html#autotoc_md23", [
+        [ "Step 1: Creating a custom nameplate", "_nameplate_setup.html#autotoc_md24", null ],
+        [ "Step 2: Adding the nameplate to your character", "_nameplate_setup.html#autotoc_md25", null ],
+        [ "Step 3: Add the YomNameplateManager to your MetaspacePlayer", "_nameplate_setup.html#autotoc_md26", null ]
       ] ]
     ] ],
     [ "Portal Setup", "_portal_setup.html", [
-      [ "Adding a portal to your Metaspace", "_portal_setup.html#autotoc_md25", [
-        [ "Step 1: Creating a Custom portal", "_portal_setup.html#autotoc_md26", null ],
-        [ "Step 2: Adding the portal to your level", "_portal_setup.html#autotoc_md27", null ],
-        [ "Step 3: Adding a button to your level", "_portal_setup.html#autotoc_md28", null ],
-        [ "Step 4: Adding the button to your level", "_portal_setup.html#autotoc_md29", null ],
-        [ "Step 5: Opening the portal", "_portal_setup.html#autotoc_md30", null ],
-        [ "Step 6: Checking the images that are generated", "_portal_setup.html#autotoc_md31", null ]
+      [ "Adding a portal to your Metaspace", "_portal_setup.html#autotoc_md27", [
+        [ "Step 1: Creating a Custom portal", "_portal_setup.html#autotoc_md28", null ],
+        [ "Step 2: Adding the portal to your level", "_portal_setup.html#autotoc_md29", null ],
+        [ "Step 3: Adding a button to your level", "_portal_setup.html#autotoc_md30", null ],
+        [ "Step 4: Adding the button to your level", "_portal_setup.html#autotoc_md31", null ],
+        [ "Step 5: Opening the portal", "_portal_setup.html#autotoc_md32", null ],
+        [ "Step 6: Checking the images that are generated", "_portal_setup.html#autotoc_md33", null ]
       ] ]
     ] ],
     [ "Airdrop Setup", "_airdrop_setup.html", [
-      [ "Adding airdrop functionality to your metaverse", "_airdrop_setup.html#autotoc_md32", [
-        [ "Step 1: Using the airdrop node", "_airdrop_setup.html#autotoc_md33", null ]
+      [ "Adding airdrop functionality to your metaverse", "_airdrop_setup.html#autotoc_md34", [
+        [ "Step 1: Using the airdrop node", "_airdrop_setup.html#autotoc_md35", null ]
       ] ]
     ] ],
     [ "Counter Setup", "_counter_setup.html", [
-      [ "Adding a counter to your metaverse", "_counter_setup.html#autotoc_md34", [
-        [ "Step 1: Adding counter actor the the world", "_counter_setup.html#autotoc_md35", null ],
-        [ "Step 2: Adding counter component to the player", "_counter_setup.html#autotoc_md36", null ],
-        [ "Step 3: Using blueprint functions to use the component", "_counter_setup.html#autotoc_md37", null ]
+      [ "Adding a counter to your metaverse", "_counter_setup.html#autotoc_md36", [
+        [ "Step 1: Adding counter actor the the world", "_counter_setup.html#autotoc_md37", null ],
+        [ "Step 2: Adding counter component to the player", "_counter_setup.html#autotoc_md38", null ],
+        [ "Step 3: Using blueprint functions to use the component", "_counter_setup.html#autotoc_md39", null ]
       ] ]
     ] ],
     [ "Gated Access Setup", "_gated_access_setup.html", [
-      [ "Adding a Gated Access to your metaverse", "_gated_access_setup.html#autotoc_md38", [
-        [ "Step 1: Adding Gated Access component", "_gated_access_setup.html#autotoc_md39", null ],
-        [ "Step 2: Adding Gated Access actor to the level", "_gated_access_setup.html#autotoc_md40", null ],
-        [ "Step 3: Set Gated Access Item ID", "_gated_access_setup.html#autotoc_md41", null ],
-        [ "Example of Gated Access", "_gated_access_setup.html#autotoc_md42", null ]
+      [ "Adding a Gated Access to your metaverse", "_gated_access_setup.html#autotoc_md40", [
+        [ "Step 1: Adding Gated Access component", "_gated_access_setup.html#autotoc_md41", null ],
+        [ "Step 2: Adding Gated Access actor to the level", "_gated_access_setup.html#autotoc_md42", null ],
+        [ "Step 3: Set Gated Access Item ID", "_gated_access_setup.html#autotoc_md43", null ],
+        [ "Example of Gated Access", "_gated_access_setup.html#autotoc_md44", null ]
       ] ]
     ] ],
     [ "Control Hints Setup", "_control_hints_setup.html", [
-      [ "Displaying the Control Hints Panel to the player's screen", "_control_hints_setup.html#autotoc_md43", [
-        [ "Step 1: Adding Control Hints component", "_control_hints_setup.html#autotoc_md44", null ],
-        [ "Example of ControlHints", "_control_hints_setup.html#autotoc_md45", null ]
+      [ "Displaying the Control Hints Panel to the player's screen", "_control_hints_setup.html#autotoc_md45", [
+        [ "Step 1: Adding Control Hints component", "_control_hints_setup.html#autotoc_md46", null ],
+        [ "Example of ControlHints", "_control_hints_setup.html#autotoc_md47", null ]
       ] ]
     ] ],
     [ "External events", "_external_events.html", [
-      [ "External events", "_external_events.html#autotoc_md46", [
-        [ "Creating your own events", "_external_events.html#autotoc_md47", null ],
-        [ "Calling your own events", "_external_events.html#autotoc_md48", null ]
+      [ "External events", "_external_events.html#autotoc_md48", [
+        [ "Creating your own events", "_external_events.html#autotoc_md49", null ],
+        [ "Calling your own events", "_external_events.html#autotoc_md50", null ]
       ] ]
     ] ],
     [ "Package Unreal Map Data", "_package_map_data.html", [
-      [ "Package Unreal Map Data", "_package_map_data.html#autotoc_md49", [
-        [ "Step 1: Build your map data", "_package_map_data.html#autotoc_md50", null ],
-        [ "Step 2: Package your map data", "_package_map_data.html#autotoc_md51", null ],
-        [ "Step 3: Upload your map data to github", "_package_map_data.html#autotoc_md52", null ],
-        [ "Result", "_package_map_data.html#autotoc_md53", null ]
+      [ "Package Unreal Map Data", "_package_map_data.html#autotoc_md51", [
+        [ "Step 1: Build your map data", "_package_map_data.html#autotoc_md52", null ],
+        [ "Step 2: Package your map data", "_package_map_data.html#autotoc_md53", null ],
+        [ "Step 3: Upload your map data to github", "_package_map_data.html#autotoc_md54", null ],
+        [ "Result", "_package_map_data.html#autotoc_md55", null ]
       ] ]
     ] ]
 ];
--- html/search/pages_7.js
+++ html/search/pages_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['package_20unreal_20map_20data_0',['Package Unreal Map Data',['../_package_map_data.html',1,'md_pages_Guides']]],
-  ['portal_20setup_1',['Portal Setup',['../_portal_setup.html',1,'md_pages_Guides']]]
+  ['performance_20recommendations_1',['Performance Recommendations',['../_performance_recommendations.html',1,'md_pages_Guides']]],
+  ['portal_20setup_2',['Portal Setup',['../_portal_setup.html',1,'md_pages_Guides']]]
 ];

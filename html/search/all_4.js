e4c1b9e132b4b48d4124717f3d68cc5f630e575f
KungFuDonkey <sietze.riemersma@gmail.com>
2023-05-04 13:11:42 +0000

    update of documentation

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['emittrackinginfo_0',['EmitTrackingInfo',['../class_u_yom_tracking_b_p_functions.html#aa612d7391239fdc29e1456a25d721757',1,'UYomTrackingBPFunctions']]],
-  ['eventname_1',['EventName',['../class_u_yom_external_event.html#ad0b4d6cd4db19c1c0b5290dc827b6bf9',1,'UYomExternalEvent::EventName()'],['../class_u_yom_stream_event.html#ac308960b72a8e05168a1919e165d93b5',1,'UYomStreamEvent::EventName()']]],
-  ['ewallettype_2',['EWalletType',['../group___yom_core.html#gaffa0fbc759e8fa855b98981147f5b242',1,'YomWallets.h']]],
-  ['external_20events_3',['External events',['../_external_events.html',1,'md_pages_Guides']]],
-  ['external_5fevent_5fmodule_5fname_4',['EXTERNAL_EVENT_MODULE_NAME',['../_yom_client_external_event_handler_8h.html#a5fda2f956b56aad52a70eff8aa66b218',1,'YomClientExternalEventHandler.h']]],
-  ['externalevent_5',['ExternalEvent',['../class_u_yom_external_event.html#a4cdb809c6c29672a3eeead5f67a847a2',1,'UYomExternalEvent']]]
+  ['emittrackinginfogoogleanalytics_1',['EmitTrackingInfoGoogleAnalytics',['../class_u_yom_tracking_b_p_functions.html#a407e8bc505d930583eb5e488c47015ae',1,'UYomTrackingBPFunctions']]],
+  ['eventname_2',['EventName',['../class_u_yom_external_event.html#ad0b4d6cd4db19c1c0b5290dc827b6bf9',1,'UYomExternalEvent::EventName()'],['../class_u_yom_stream_event.html#ac308960b72a8e05168a1919e165d93b5',1,'UYomStreamEvent::EventName()']]],
+  ['ewallettype_3',['EWalletType',['../group___yom_core.html#gaffa0fbc759e8fa855b98981147f5b242',1,'YomWallets.h']]],
+  ['external_20events_4',['External events',['../_external_events.html',1,'md_pages_Guides']]],
+  ['external_5fevent_5fmodule_5fname_5',['EXTERNAL_EVENT_MODULE_NAME',['../_yom_client_external_event_handler_8h.html#a5fda2f956b56aad52a70eff8aa66b218',1,'YomClientExternalEventHandler.h']]],
+  ['externalevent_6',['ExternalEvent',['../class_u_yom_external_event.html#a4cdb809c6c29672a3eeead5f67a847a2',1,'UYomExternalEvent']]]
 ];
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['emittrackinginfo_0',['EmitTrackingInfo',['../class_u_yom_tracking_b_p_functions.html#aa612d7391239fdc29e1456a25d721757',1,'UYomTrackingBPFunctions']]]
+  ['emittrackinginfo_0',['EmitTrackingInfo',['../class_u_yom_tracking_b_p_functions.html#aa612d7391239fdc29e1456a25d721757',1,'UYomTrackingBPFunctions']]],
+  ['emittrackinginfogoogleanalytics_1',['EmitTrackingInfoGoogleAnalytics',['../class_u_yom_tracking_b_p_functions.html#a407e8bc505d930583eb5e488c47015ae',1,'UYomTrackingBPFunctions']]]
 ];
